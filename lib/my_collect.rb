401135d95d8475a115b208be48ef5890f2fc282a
Nicholas  Luong <nckluong@gmail.com>
2018-08-18 01:20:39 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect(collection)
+  count = 0 
+  newCollection = []
+  while count < collection.size 
+    newCollection << yield(collection[count])
+    count += 1 
+  end 
+    newCollection 
+end 
+    
